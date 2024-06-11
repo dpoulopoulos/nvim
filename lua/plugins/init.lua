8b8a0205b82b06e041725f07e55b8c88918dac44
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-06-11 10:50:23 +0300

    LSP: Enable language server support
    Add support for:
    
    * Python
    * Go
    * Docker
    * YAML
    * JSON
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

---
+++ lua/plugins/init.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    -- Lightweight yet powerful formatter plugin for Neovim
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+        "pyright",
+  			"lua-language-server",
+        "dockerfile-language-server",
+  			"html-lsp",
+        "css-lsp",
+        "debugpy",
+        "prettier",
+        "flake8",
+        "stylua",
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			-- defaults
+        "vim",
+        "lua",
+
+        -- dev
+        "python",
+        "go",
+
+        -- config
+        "json",
+        "yaml",
+
+        -- other
+        "vimdoc",
+        "html",
+        "css"
+  		},
+  	},
+  },
+}
