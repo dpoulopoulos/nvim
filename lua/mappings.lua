53ab70fea6c013eeba6aa828a61edeafd0c24742
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-06-29 12:46:44 +0300

    mappings: Remap tmux navigator
    Remap tmux navigator to use the Alt key instead of Ctrl, and remap the
    horizontal terminal window to `Alt + t` to avoid conflicts.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,9 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("n", "<M-h>", "<cmd> TmuxNavigateLeft <cr>")
-map("n", "<M-j>", "<cmd> TmuxNavigateDown <cr>")
-map("n", "<M-k>", "<cmd> TmuxNavigateUp <cr>")
-map("n", "<M-l>", "<cmd> TmuxNavigateRight <cr>")
+-- map navigation between tmux panes
+map("n", "<A-h>", "<cmd> TmuxNavigateLeft <cr>")
+map("n", "<A-j>", "<cmd> TmuxNavigateDown <cr>")
+map("n", "<A-k>", "<cmd> TmuxNavigateUp <cr>")
+map("n", "<A-l>", "<cmd> TmuxNavigateRight <cr>")
+
+-- remap horizontal terminal
+map({ "n", "t" }, "<A-t>", function ()
+  require("nvchad.term").toggle { pos = "sp", id = "horizontalTerm"}
+end)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
