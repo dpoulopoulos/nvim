e6ab5789d7effc2161f0b981dd66d5649c2c434e
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-12-15 15:12:45 +0200

    Auto-reload files when modified externally
    See: https://unix.stackexchange.com/a/383044
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,13 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+local api = vim.api
+
+-- auto-reload files when modified externally
+-- https://unix.stackexchange.com/a/383044
+o.autoread = true
+api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
+})
