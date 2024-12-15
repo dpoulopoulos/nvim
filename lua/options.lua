7a3b2f50d8cbfe06956351824a2fd5cf159f4e18
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2024-12-15 19:33:56 +0200

    Set wrap and spell
    Set wrap and spell in markdown and git commit messages.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -14,3 +14,13 @@ api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained"
 	command = "if mode() != 'c' | checktime | endif",
 	pattern = { "*" },
 })
+
+-- Set wrap and spell in markdown and gitcommit
+api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
