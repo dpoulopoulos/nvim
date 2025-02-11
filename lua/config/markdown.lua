4eba71ac63c3c7dd990abfdec7718a4e3a15940b
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2025-02-11 21:28:37 +0200

    Configure Obsidian plugin
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- lua/config/markdown.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.bo.formatoptions = "tcroql" -- 't' uses textwidth, other flags for comments/auto-format
-  end,
-})
