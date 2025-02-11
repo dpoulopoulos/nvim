c3923dc65d0cdd5ec5568dc6885aaa8e0d40b4dd
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2025-02-11 16:46:58 +0200

    Add integration with tmux
    Enable the `vim-tmux-navigator` plugin to integrate tmux with Neovim.
    
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- lua/plugins/dimpo.lua
+++ lua/plugins/dimpo.lua
@@ -1,18 +1,41 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "zettelkasten",
-        path = "/home/dimpo/onedrive/Documents/zettelkasten",
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+
+    opts = {
+      workspaces = {
+        {
+          name = "zettelkasten",
+          path = "/home/dimpo/onedrive/Documents/zettelkasten",
+        },
       },
+    },
+  },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+      { "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
     },
   },
 }
