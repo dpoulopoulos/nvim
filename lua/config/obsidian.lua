2e94b88fdb1028b4ce47a766ecd1c2a93a1315c8
Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>
2025-02-11 11:29:13 +0200

    Configure obsidian plugin
    Signed-off-by: Dimitris Poulopoulos <dimitris.a.poulopoulos@gmail.com>

--- lua/config/obsidian.lua
+++ lua/config/obsidian.lua
@@ -20,8 +20,6 @@ require("obsidian").setup({
     template = "journal.md",
   },
 
-  -- disable_frontmatter = true,
-
   -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
   completion = {
     -- Set to false to disable completion.
@@ -51,5 +49,31 @@ require("obsidian").setup({
       end,
       opts = { buffer = true },
     },
+  },
+
+  -- Specify how to handle attachments.
+  attachments = {
+    -- The default folder to place images in via `:ObsidianPasteImg`.
+    -- If this is a relative path it will be interpreted as relative to the vault root.
+    -- You can always override this per image by passing a full path to the command instead of just a filename.
+    img_folder = "_images", -- This is the default
+
+    -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
+    ---@return string
+    img_name_func = function()
+      -- Prefix image names with timestamp.
+      return string.format("%s-", os.time())
+    end,
+
+    -- A function that determines the text to insert in the note when pasting an image.
+    -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+    -- This is the default implementation.
+    ---@param client obsidian.Client
+    ---@param path obsidian.Path the absolute path to the image file
+    ---@return string
+    img_text_func = function(client, path)
+      path = client:vault_relative_path(path) or path
+      return string.format("![%s](%s)", path.name, path)
+    end,
   },
 })
